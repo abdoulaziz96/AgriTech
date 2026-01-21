e40a8198f0264549b0cc5aebcf4d5bc8712bda8a
TIDJANI  Abdoul Aziz <tidjaniabdoulaziz96@gmail.com>
2026-01-21 17:58:36 +0100

    Modification

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'tidjaniabdoulaziz96@gmail.com', '1234567890') if not User.objects.filter(username='admin').exists() else None"
